66bbdd22ead7175a3279fb9154703e92743ca2d4
Anatolii Prylutskyi <anpryl@gmail.com>
2018-06-14 14:24:20 +0300

    Update timeguard build scripts

--- timeguard/run.sh
+++ timeguard/run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
 xhost +si:localuser:root && \
-sudo docker run -d -ti --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix timeguard
+sudo docker run -d -ti --rm \
+  -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  timeguard
